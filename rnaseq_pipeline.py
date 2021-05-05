fd462ab9d822927d69883d74a054b22da1454314
asgerjakobsen <asger.jakobsen@rdm.ox.ac.uk>
2021-05-05 20:31:09 +0100

    tidied up naming

--- rnaseq_pipeline.py
+++ rnaseq_pipeline.py
@@ -26,7 +26,7 @@ def fastqc(input_file, output_file):
 ## Merge fastq files where they separated by lane
 @follows(mkdir('2_trimmed'))
 @follows(fastqc)
-@active_if(P.get_params()['input'] == "PE_novogene")
+@active_if(P.get_params()['input'] == "PE_barcoded")
 @collate('fastq/*q.gz', regex(r'fastq/(.*?)_(.*)_(L00\d)_(R[12])_001.f.*q.gz'), r'2_trimmed/\1.merged_\4.fq.gz')
 def merge_lanes(input_files, output_file):
     files = ' '.join(str(x) for x in input_files)
@@ -38,14 +38,14 @@ def merge_lanes(input_files, output_file):
 ##### Trimming #####
 
 @follows(mkdir('2_trimmed'))
-## SE_illumina: single-end reads with file extension in the format: R1_001.fastq.gz
-@active_if(P.get_params()['input'] == "SE_illumina")
+## SE_demultiplexed: single-end reads with file extension in the format: R1_001.fastq.gz
+@active_if(P.get_params()['input'] == "SE_demultiplexed")
 @transform('fastq/*.fastq.gz', regex(r'fastq/(.*)_(.*)_R1_001.fastq.gz'), r'2_trimmed/\1/\1.trimmed.fq.gz')
 def trimming_SE(input_file, output_file):
     basename = P.snip(os.path.basename(input_file),"_R1_001.fastq.gz").split("_")[0]
     statement = '''mkdir 2_trimmed/%(basename)s &&
-    mkdir -p 4_mapping/%(basename)s &&
-    mkdir -p 5_mapping_qc/%(basename)s &&
+    mkdir -p 3_mapping/%(basename)s &&
+    mkdir -p 4_mapping_qc/%(basename)s &&
     trim_galore --cores 4 -q %(trimgalore_q)s -a "A{100}"
     %(trimgalore_options)s
     %(input_file)s  --basename %(basename)s -o 2_trimmed
@@ -53,20 +53,19 @@ def trimming_SE(input_file, output_file):
     P.run(statement, job_queue=PARAMS['q'], job_threads=4)
 
 
-## PE_novogene: paired-end reads with file extension in the format: L001_R1_001.fastq.gz
+## PE_barcoded: paired-end reads with file extension in the format: L001_R1_001.fastq.gz
 @follows(mkdir('2_trimmed'))
 @follows(merge_lanes)
-@active_if(P.get_params()['input'] == "PE_novogene")
+@active_if(P.get_params()['input'] == "PE_barcoded")
 @subdivide('2_trimmed/*merged_R1.fq.gz', regex(r"2_trimmed/(.*).merged_R1.fq.gz"), output = r"2_trimmed/\1/\1_HT*.trimmed_R1.fq.gz")
 def trimming_PE(input_file, output_files):
     input_file2 = input_file.replace("_R1.fq.gz", "_R2.fq.gz")
-    #output_file2 = output_files.replace(".trimmed_R1.fq.gz", ".trimmed_R2.fq.gz")
     outfile_prefix = P.snip(input_file,".merged_R1.fq.gz").split(".")[0]
     basename = P.snip(os.path.basename(input_file),".merged_R1.fq.gz").split("_")[0]
     #lane_suffix = input_file,r"_(.*)_L00\d_R1.fq.gz").split("_")[0]
     statement = '''mkdir 2_trimmed/%(basename)s &&
-    mkdir -p 4_mapping/%(basename)s &&
-    mkdir -p 5_mapping_qc/%(basename)s &&
+    mkdir -p 3_mapping/%(basename)s &&
+    mkdir -p 4_mapping_qc/%(basename)s &&
     cutadapt --cores=0
     -g file:%(cutadapt_barcodes)s
     --nextseq-trim=%(cutadapt_q)s
@@ -87,15 +86,15 @@ def trimming_PE(input_file, output_files):
 
 ##### Mapping #####
 
-@follows(mkdir('4_mapping'))
+@follows(mkdir('3_mapping'))
 @follows(trimming_SE)
 @follows(trimming_PE)
 @follows(merge_lanes)
-@transform('2_trimmed/*/*.fq.gz', regex(r"2_trimmed/(.*)/(.*).trimmed(_R2)?.fq.gz"), r"4_mapping/\1/\2.bam")
+@transform('2_trimmed/*/*.fq.gz', regex(r"2_trimmed/(.*)/(.*).trimmed(_R2)?.fq.gz"), r"3_mapping/\1/\2.bam")
 def star(input_file, output_file):
     outprefix = P.snip(output_file, ".bam")
     # SE mapping
-    if P.get_params()['mapping'] == "SE_illumina":
+    if P.get_params()['mapping'] == "SE_demultiplexed":
         statement = '''STAR
         --runThreadN %(star_threads)s
         --genomeDir %(star_ref)s
@@ -109,8 +108,8 @@ def star(input_file, output_file):
         if P.get_params()["zap_files"]==1:
             IOTools.zap_file(input_file)
     
-    # SE mapping of PE_novogene reads
-    elif P.get_params()['input'] == "PE_novogene" and P.get_params()['mapping'] == "read2":
+    # SE mapping of PE_barcoded reads
+    elif P.get_params()['input'] == "PE_barcoded" and P.get_params()['mapping'] == "read2":
         read1 = input_file.replace("_R2.fq.gz", "_R1.fq.gz")
         #basename = P.snip(os.path.basename(input_file),".trimmed_2.fq.gz").split("_")[0]
         statement = '''STAR
@@ -128,7 +127,7 @@ def star(input_file, output_file):
             IOTools.zap_file(read1)
             
     # PE mapping
-    elif P.get_params()['input'] == "PE_novogene" and P.get_params()['mapping'] == "both":
+    elif P.get_params()['input'] == "PE_barcoded" and P.get_params()['mapping'] == "both":
         read1 = input_file.replace("_R2.fq.gz", "_R1.fq.gz")
         statement = '''STAR
         --runThreadN %(star_threads)s
@@ -157,14 +156,14 @@ def bam_index(input_file, output_file):
 ##### Mapping QC #####
     
 @follows(bam_index)
-@follows(mkdir('5_mapping_qc'))  
-@transform(star, regex(r'4_mapping/(.*)/(.*).bam'), r'5_mapping_qc/\1/\2.idxstat')
+@follows(mkdir('4_mapping_qc'))
+@transform(star, regex(r'3_mapping/(.*)/(.*).bam'), r'4_mapping_qc/\1/\2.idxstat')
 def samtools_idxstat(input_file, output_file):
     statement = '''samtools idxstats %(input_file)s > %(output_file)s'''
     P.run(statement, job_queue=PARAMS['q'], job_memory = '100M')
 
 @follows(bam_index, samtools_idxstat)
-@transform(star, regex(r'4_mapping/(.*)/(.*).bam'), r'5_mapping_qc/\1/\2.flagstat')
+@transform(star, regex(r'3_mapping/(.*)/(.*).bam'), r'4_mapping_qc/\1/\2.flagstat')
 def samtools_flagstat(input_file, output_file):
     statement = '''samtools flagstat %(input_file)s > %(output_file)s'''
     P.run(statement, job_queue=PARAMS['q'], job_memory = '100M')
@@ -173,23 +172,23 @@ def samtools_flagstat(input_file, output_file):
 ##### Featurecounts #####
     
 @follows(bam_index)
-@follows(mkdir('6_featurecounts'))
-@merge(star, '6_featurecounts/counts.txt')
+@follows(mkdir('5_featurecounts'))
+@merge(star, '5_featurecounts/counts.txt')
 def count_reads(input_files, output_file):
     input_files_string = ' '.join(input_files)
     statement = '''featureCounts -T 12 -t exon -g gene_id
     -a %(featurecounts_gtf)s -o %(output_file)s %(input_files_string)s
     %(featurecounts_options)s
-    2> 6_featurecounts/featurecounts_log.txt'''
+    2> 5_featurecounts/featurecounts_log.txt'''
     P.run(statement, job_queue=PARAMS['q'], job_threads=12, job_memory = '8G')
 
 ## Multiqc report ##
 
 @follows(count_reads, samtools_idxstat)
 #count_reads only there to specify it should be done at end
-@merge(samtools_flagstat, '7_multiqc/multiqc_report.html')
+@merge(samtools_flagstat, '6_multiqc/multiqc_report.html')
 def multiqc(input_file, output_file):
-    statement = '''multiqc . -o 7_multiqc'''
+    statement = '''multiqc . -o 6_multiqc'''
     P.run(statement, job_queue=PARAMS['q'], job_memory = '8G')
 
 
